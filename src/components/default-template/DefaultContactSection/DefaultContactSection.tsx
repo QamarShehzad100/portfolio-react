b40794dda33ae4b104b9fecd93bf673696a78764
Qamar Shehzad <qamarshehzad100@gmail.com>
2024-07-24 13:08:36 +0500

    contact section and footer updated

--- src/components/default-template/DefaultContactSection/DefaultContactSection.tsx
+++ src/components/default-template/DefaultContactSection/DefaultContactSection.tsx
@@ -1,12 +1,17 @@
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FaWhatsapp } from "react-icons/fa"; // Import WhatsApp icon from react-icons
 
 import { EMAIL_ADDRESS, GITHUB_URL, LINKEDIN_URL } from "constants/links";
 import GithubIcon from "icons/GithubIcon";
 import LinkedinIcon from "icons/LinkedinIcon";
 import MailIcon from "icons/MailIcon";
+// Import WhatsApp icon
 import { Section } from "shared/Section";
 
 import { DefaultContactItem } from "./DefaultContactItem";
+
+// Add WhatsApp number constant
+const WHATSAPP_NUMBER = "03445208419";
 
 export const DefaultContactSection = () => {
   return (
@@ -20,6 +25,7 @@ export const DefaultContactSection = () => {
             buttonText="Copy to clipboard"
           />
         </CopyToClipboard>
+        <DefaultContactItem icon={<FaWhatsapp />} title="WhatsApp" text={WHATSAPP_NUMBER} />
         <DefaultContactItem icon={<LinkedinIcon />} title="LinkedIn" href={LINKEDIN_URL} />
         <DefaultContactItem icon={<GithubIcon />} title="GitHub" href={GITHUB_URL} />
       </div>
--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -4,8 +4,8 @@ export const Footer = () => {
   return (
     <footer className="flex flex-col py-[64px] px-[24px] bg-background2 border-t border-border1">
       <div className="flex flex-col max-w-[1240px] w-full mx-auto">
-        <Typography className="text-l">
-          Copyright &copy; {new Date().getFullYear()} Adrian Piątek. All Rights Reserved.
+        <Typography className="text-l text-center">
+          Copyright &copy; {new Date().getFullYear()} Qamar Shehzad. All Rights Reserved.
         </Typography>
       </div>
     </footer>
