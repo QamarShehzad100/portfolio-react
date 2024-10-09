c651af4ab7fa73481bb1c5f404afb52d7cf67a54
Qamar Shehzad <qamarshehzad100@gmail.com>
2024-10-09 19:50:45 +0500

    heading text updated

--- src/components/shared/Section/Section.tsx
+++ src/components/shared/Section/Section.tsx
@@ -10,10 +10,14 @@ interface SectionProps {
 
 export const Section = ({ children, id, headingText }: SectionProps) => {
   return (
-    <section id={id} className="flex flex-col">
-      <div className="flex flex-col gap-[40px] w-full my-[84px] mx-auto sm:my-[124px]">
-        <div className="animate-hidden flex flex-col w-full h-full">
-          <Typography tag="h2" weight="semibold" className="text-5xl sm:text-7xl">
+    <section id={id} className="flex flex-col ">
+      <div className="flex flex-col gap-[40px] w-full my-[84px] mx-auto sm:my-[124px] ">
+        <div className="animate-hidden flex flex-col w-full h-full text-xl">
+          <Typography
+            tag="h2"
+            weight="semibold"
+            className="text-4xl sm:text-4xl md:text-5xl lg:text-6xl"
+          >
             {headingText}
           </Typography>
         </div>
