92742094bd3233ae44dc08936793d0c0677ac92c
Qamar Shehzad <qamarshehzad100@gmail.com>
2024-10-09 17:32:09 +0500

    deployment null error resolved

--- src/components/default-template/DefaultProjectsSection/DefaultTypingAnimation.tsx
+++ src/components/default-template/DefaultProjectsSection/DefaultTypingAnimation.tsx
@@ -3,21 +3,27 @@ import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
 const DefaultTypingAnimation = () => {
-  const textRef = useRef(null);
+  const textRef = useRef<HTMLSpanElement | null>(null); // Add proper typing for textRef
   const roles = ["Qamar Shehzad", "Frontend Developer", "JS Developer", "React Developer"];
 
   useEffect(() => {
     let index = 0;
     const changeText = () => {
-      gsap.to(textRef.current, {
-        opacity: 0,
-        duration: 0.5,
-        onComplete: () => {
-          textRef.current.textContent = roles[index];
-          gsap.to(textRef.current, { opacity: 1, duration: 0.5 });
-          index = (index + 1) % roles.length;
-        }
-      });
+      if (textRef.current) {
+        // Check if textRef.current is not null
+        gsap.to(textRef.current, {
+          opacity: 0,
+          duration: 0.5,
+          onComplete: () => {
+            if (textRef.current) {
+              // Check again inside the callback
+              textRef.current.textContent = roles[index];
+              gsap.to(textRef.current, { opacity: 1, duration: 0.5 });
+              index = (index + 1) % roles.length;
+            }
+          }
+        });
+      }
     };
 
     // Call changeText immediately to display the first role
@@ -31,7 +37,7 @@ const DefaultTypingAnimation = () => {
   return (
     <div className="flex items-center space-x-2">
       <span className="whitespace-nowrap border-r-2 border-gray-600 pr-2 animate-typing">I'm</span>
-      <span className="opacity-0 transition-opacity duration-500 text-primary " ref={textRef}>
+      <span className="opacity-0 transition-opacity duration-500 text-primary" ref={textRef}>
         Qamar Shehzad
       </span>
     </div>
